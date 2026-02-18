ab47915750eff58197bf167569eb814b0aa381a0
Dan Stefaniuk <daniel.stefaniuk@gmail.com>
2026-02-18 08:11:43 +0000

    fix(script): remove conditional check for assets directory and always clone repository template

--- .github/skills/repository-template/scripts/git-clone-repository-template.sh
+++ .github/skills/repository-template/scripts/git-clone-repository-template.sh
@@ -2,9 +2,8 @@
 
 cd "$(git rev-parse --show-toplevel)" || exit 1
 cd .github/skills/repository-template
-if ! [ -d "assets" ]; then
-  git clone https://github.com/stefaniuk/repository-template.git assets
-fi
+rm -rf assets
+git clone https://github.com/stefaniuk/repository-template.git assets
 cd assets
 git pull origin custom
 git checkout custom
